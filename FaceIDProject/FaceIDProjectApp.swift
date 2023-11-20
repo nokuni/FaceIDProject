653612c837fba86b464b72b41a3771efd022401e
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-11-20 13:49:35 +0100

    Initial Commit

---
+++ FaceIDProject/FaceIDProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDProjectApp.swift
+//  FaceIDProject
+//
+//  Created by Yann Christophe Maertens on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
